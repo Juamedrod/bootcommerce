a61bc0310760cb3006b905a85f48fa99eefa343e
JM_Medina <juamedrod@hotmail.es>
2022-01-03 11:55:36 +0100

    Common point start

--- src/services/services.js
+++ src/services/services.js
@@ -1,7 +1,2 @@
-//aquí van todos los servicios de la aplicación, si algo usa algo externo a REact debe ir aquí. ejemplos: Llamadas a las APIs, gestor de contenidos, filtros, etc etc. Todas las llamadas de librerias externas a react deberian ser un servicio. fat model skinny controller y le hago todas las perrerias que me dan la gana.
+//aquí van todos los servicios de la aplicación, si algo usa algo externo a REact debe ir aquí. ejemplos: Llamadas a las APIs, gestor de contenidos, filtros, etc etc. Todas las llamadas de librerias externas a react deberian ser un servicio. fat model skinny controller.
 
-//Hola service.
-
-// Pues ya estaría.
-
-//he hecho el mejor boton de la historia de react
