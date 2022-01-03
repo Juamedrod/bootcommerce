eb8b4144388e70144be52a3639db327ccd914f20
Mingorance777 <cmingoranceespigares@gmail.com>
2022-01-03 11:34:14 +0100

    Hola mundo

--- src/components/jerarquiadecomponentes.js
+++ src/components/jerarquiadecomponentes.js
@@ -1,1 +1,2 @@
 //Aquí va toda la jerarquia de los componentes , se pueden crear carpetas para organizar el trabajo. Cada uno cree una carpeta con su tarea y los componentes que necesita..
+// Hola mundo
