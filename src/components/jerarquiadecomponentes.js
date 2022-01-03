e81dded2c2fbfdf6d381f368d0e66550e3db8ca4
leost28 <leost28@gmail.com>
2022-01-03 11:33:32 +0100

    algo bonito

--- src/components/jerarquiadecomponentes.js
+++ src/components/jerarquiadecomponentes.js
@@ -1,2 +1,4 @@
 //Aquí va toda la jerarquia de los componentes , se pueden crear carpetas para organizar el trabajo. Cada uno cree una carpeta con su tarea y los componentes que necesita..
 // Hola mundo
+
+//Algo bonito jeje
